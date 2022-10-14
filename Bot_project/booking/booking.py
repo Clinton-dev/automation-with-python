cad69f30db6de153a824b863a9c470d6dd218469
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-14 13:17:33 +0300

    Add choose currency functionality

--- Bot_project/booking/booking.py
+++ Bot_project/booking/booking.py
@@ -3,11 +3,23 @@ import booking.constants as const
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self):
+    def __init__(self, teardown=False):
+        self.teardown = teardown
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc, traceback):
-        self.quit()
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def choose_currency(self, currency='USD'):
+        currency_element = self.find_element(
+            'css selector', 'button[data-tooltip-text="Choose your currency"]')
+        currency_element.click()
+        selected_currency = self.find_element(
+            'css selector', f'a[data-modal-header-async-url-param*="changed_currency=1&selected_currency={currency}"]')
+        selected_currency.click()
--- Bot_project/run.py
+++ Bot_project/run.py
@@ -1,5 +1,6 @@
 from booking.booking import Booking
 
-
+# teardown=True
 with Booking() as bot:
     bot.land_first_page()
+    # bot.choose_currency(currency='KES')
