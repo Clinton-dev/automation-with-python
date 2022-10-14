717b939e6f580d5d02dd084115983c54abb3e235
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-14 11:20:26 +0300

    use python context manager to instanciate the booking class

--- Bot_project/booking/booking.py
+++ Bot_project/booking/booking.py
@@ -6,5 +6,8 @@ class Booking(webdriver.Chrome):
     def __init__(self):
         super(Booking, self).__init__()
 
+    def __exit__(self, exc_type, exc, traceback):
+        self.quit()
+
     def land_first_page(self):
         self.get(const.BASE_URL)
--- Bot_project/run.py
+++ Bot_project/run.py
@@ -1,4 +1,5 @@
 from booking.booking import Booking
 
-inst = Booking()
-inst.land_first_page()
+
+with Booking() as bot:
+    bot.land_first_page()
