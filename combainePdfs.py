df4e85a20b58b001996b20a05c4d3d3c7394a9d0
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-25 10:59:07 +0300

    Add combinepdf.py

---
+++ combainePdfs.py
@@ -0,0 +1,35 @@
+#! python 3.8.10
+""" Combaine all pdfs in working directory into one file """
+
+import PyPDF2
+import os
+
+pdfFiles = []
+
+for filename in os.listdir():
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key=str.lower)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pdfFile in pdfFiles:
+    file = open(pdfFile, 'rb')
+    path = file.name
+    # check to see if file is not empty
+    if (os.stat(file.name).st_size != 0):
+        pdfReader = PyPDF2.PdfFileReader(file.name)
+
+        # check to see if file is decrypted
+        if (pdfReader.isEncrypted):
+            print(f'{path} is encrypted')
+        else:
+            for pageNum in range(pdfReader.numPages):
+                if pageNum != 0:
+                    pdfWriter.addPage(pdfReader.getPage(pageNum))
+
+resultPdf = open('allminutes.pdf', 'wb')
+pdfWriter.write(resultPdf)
+resultPdf.close()
+print("***Finished combaning pdf Files***")
+print("check allminutes.pdf")
