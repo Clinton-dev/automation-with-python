12ccfb86c54b57b4cad7f6ed0e5e559d9e497bdb
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-28 14:27:13 +0300

    Add combinecsv.py file

---
+++ combaineCsvs.py
@@ -0,0 +1,32 @@
+# ! python 3.8.10
+# combaineCsvs.py- combines all csv files stripped off their headers into one file
+
+import os
+import csv
+
+
+def find_all_csv():
+    csvFiles = []
+
+    for filename in os.listdir('./headerRemoved'):
+        if filename.endswith('.csv'):
+            csvFiles.append(filename)
+
+    return csvFiles
+
+
+def combine_all_csvs():
+    csvFiles = find_all_csv()
+
+    csvWriter = csv.writer(
+        open('headerRemoved/allCsv.csv', 'w'))
+
+    for filename in csvFiles:
+        csvFileObj = open(filename)
+        csvReader = csv.reader(csvFileObj)
+
+        for row in csvReader:
+            csvWriter.writerow(row)
+
+
+# combine_all_csvs()
--- combainePdfs.py
+++ combainePdfs.py
@@ -4,32 +4,43 @@
 import PyPDF2
 import os
 
-pdfFiles = []
 
-for filename in os.listdir():
-    if filename.endswith('.pdf'):
-        pdfFiles.append(filename)
+def find_pdffiles():
+    pdfFiles = []
 
-pdfFiles.sort(key=str.lower)
-pdfWriter = PyPDF2.PdfFileWriter()
+    for filename in os.listdir():
+        if filename.endswith('.pdf'):
+            pdfFiles.append(filename)
 
-for pdfFile in pdfFiles:
-    file = open(pdfFile, 'rb')
-    path = file.name
-    # check to see if file is not empty
-    if (os.stat(file.name).st_size != 0):
-        pdfReader = PyPDF2.PdfFileReader(file.name)
+    pdfFiles.sort(key=str.lower)
+    return pdfFiles
 
-        # check to see if file is decrypted
-        if (pdfReader.isEncrypted):
-            print(f'{path} is encrypted')
-        else:
-            for pageNum in range(pdfReader.numPages):
-                if pageNum != 0:
-                    pdfWriter.addPage(pdfReader.getPage(pageNum))
 
-resultPdf = open('allminutes.pdf', 'wb')
-pdfWriter.write(resultPdf)
-resultPdf.close()
-print("***Finished combaning pdf Files***")
-print("check allminutes.pdf")
+def combine_pdf():
+    pdfFiles = find_pdffiles()
+
+    pdfWriter = PyPDF2.PdfFileWriter()
+
+    for pdfFile in pdfFiles:
+        file = open(pdfFile, 'rb')
+        path = file.name
+        # check to see if file is not empty
+        if (os.stat(file.name).st_size != 0):
+            pdfReader = PyPDF2.PdfFileReader(file.name)
+
+            # check to see if file is decrypted
+            if (pdfReader.isEncrypted):
+                print(f'{path} is encrypted')
+            else:
+                for pageNum in range(pdfReader.numPages):
+                    if pageNum != 0:
+                        pdfWriter.addPage(pdfReader.getPage(pageNum))
+
+    resultPdf = open('allminutes.pdf', 'wb')
+    pdfWriter.write(resultPdf)
+    resultPdf.close()
+    print("***Finished combaning pdf Files***")
+    print(f"check allminutes.pdf")
+
+
+# combine_pdf()
